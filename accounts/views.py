9cb546db3353e0a58c88caf142c8687d08584ab6
sujana00 <sujanaamgain11@gmail.com>
2022-11-27 18:19:45 +0545

    update

--- accounts/views.py
+++ accounts/views.py
@@ -24,8 +24,11 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("home")
 
 class RegisterAPIView(APIView):
 
--- shops/models.py
+++ shops/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class ShopCategory(models.Model):
+    name = models.CharField(max_length = 300)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name 
+
+class Shop(models.Model):
+    name = models.CharField(max_length = 300)
+    category = models.Foreignkey(ShopCategory, on_delete = models.CASCADE)
+    address = models.TextField()
+    description = models.TextField()
+    email = models.EmailField(max_length = 300)
+    phone_number = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name 
